20f177fe74d5b7eea0a8528e1e56995794885228
Jocasta Manasseh-Lewis <jmanasse@Jocastas-MacBook-Pro.local>
2025-11-05 16:02:01 -0800

    confirmed downsampling code works

--- downsample_videos.py
+++ downsample_videos.py
@@ -11,30 +11,34 @@ def downsample_video(video_path, output_path, target_width=520, target_height=52
         print(f"Error opening video file: {video_path}")
         return
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_path, fourcc, 20.0, (target_width, target_height), isColor=False)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_path, fourcc, 20.0, (target_height, target_width))
     
     cv2.namedWindow("Show video", cv2.WINDOW_NORMAL) 
 
     i = 0
     while cap.isOpened():
+        ret, frame = cap.read()
         i += 1
+        if i % 1000 == 0:
+            print(f"Processed {i} frames")
         if i % 3 != 0:
             continue  # Skip frames to achieve 20Hz (assuming original is 60Hz)
-        ret, frame = cap.read()
-        print(frame[0][0])
-        cv2.imshow('Frame', frame)
+        # print(frame[0][0])
+        frame_resized = frame[(og_height)//2 - target_height//2:(og_height)//2 + target_height//2,
+                              (og_width)//2 - target_width//2:(og_width)//2 + target_width//2]
+        # cv2.imshow('Frame', frame_resized)
+        # cv2.waitKey(1) 
         if not ret:
             break
-        frame_resized = frame[(og_width)//2 - target_width//2:(og_width)//2 + target_width//2,
-                              (og_height)//2 - target_height//2:(og_height)//2 + target_height//2]
+        
         out.write(frame_resized)
 
     cap.release()
     out.release()
 
 def main():
-    downsample_video('20251103_PJA123_intruder3_day6_withphotostim20251103_17_18_04.avi','20251103_PJA123_intruder3_day6_withphotostim20251103_17_18_04_downsampled.avi')
+    downsample_video('/Users/jmanasse/Desktop/20251103_PJA123_intruder2_day6_withphotostim20251103_17_18_04.avi','/Users/jmanasse/Desktop/20251103_PJA123_intruder2_day6_withphotostim20251103_17_18_04_downsampled.avi')
 
 
 if __name__ == '__main__':
